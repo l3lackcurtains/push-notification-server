993a431349b31a9afc056aaa7f6204a5a2929620
Madhav Poudel <king.ov.universe@gmail.com>
2018-02-16 22:13:23 +0545

    Removed redundancy code

--- dist/index.js
+++ dist/index.js
@@ -16,10 +16,6 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
-var _exponentServerSdk = require('exponent-server-sdk');
-
-var _exponentServerSdk2 = _interopRequireDefault(_exponentServerSdk);
-
 var _mongoose = require('mongoose');
 
 var _mongoose2 = _interopRequireDefault(_mongoose);
@@ -38,10 +34,8 @@ var _push2 = _interopRequireDefault(_push);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var expo = new _exponentServerSdk2.default();
-
 var app = (0, _express2.default)();
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3000;
 
 // Server Setup
 app.use(_bodyParser2.default.json({ limit: '50mb' }));
@@ -54,125 +48,14 @@ _mongoose2.default.Promise = global.Promise;
 _mongoose2.default.connect(_config2.default.mdbl);
 var db = _mongoose2.default.connection;
 db.on('error', function () {
-  return console.log('[-] Failed to connect to database.');
+    return console.log('[-] Failed to connect to database.');
 }).once('open', function () {
-  return console.log('[+] Connected to database. ');
+    return console.log('[+] Connected to database. ');
 });
 
 app.use('/', _deviceTheme2.default);
 app.use('/', _push2.default);
 
-app.post('/notification', function (request, response) {
-  var _request$body = request.body,
-      token = _request$body.token,
-      title = _request$body.title,
-      description = _request$body.description,
-      delay = _request$body.delay;
-
-
-  var isPushToken = _exponentServerSdk2.default.isExponentPushToken(token);
-
-  if (isPushToken) {
-    sendPush(token, title, description, response, delay);
-  } else {
-    response.json({
-      icon: 'X',
-      message: 'Your token is invalid',
-      token: token,
-      status: 'error'
-    });
-  }
-});
-
-app.post('/welcome/:token', function (request, response) {
-  var token = request.params.token;
-  var message = 'Welcome to Rmotrgram!';
-  var description = 'Push notification with welcome message to ' + token + ' device sent';
-
-  var isPushToken = _exponentServerSdk2.default.isExponentPushToken(token);
-
-  if (isPushToken) {
-    sendPush(token, message, description, response);
-  } else {
-    response.json({
-      icon: '❌',
-      message: 'Your token is invalid',
-      token: token,
-      status: 'error'
-    });
-  }
-});
-
-app.post('/photo/:token', function (request, response) {
-  var token = request.params.token;
-  var message = 'Your photo has been successfully uploaded!';
-  var description = 'Push notification advising' + token + ' device photo is uploaded sent';
-
-  if (isPushToken) {
-    sendPush(token, message, description, response);
-  } else {
-    response.json({
-      icon: '❌',
-      message: 'Your token is invalid',
-      token: token,
-      status: 'error'
-    });
-  }
-});
-
-app.post('/sendto/:token/:from', function (request, response) {
-  var token = request.params.token;
-  var from = request.params.from;
-  var message = 'YOmotr! from ' + from;
-  var description = from + ' send yo a YOmotr';
-
-  if (isPushToken) {
-    sendPush(token, message, description, response);
-  } else {
-    response.json({
-      icon: '❌',
-      message: 'Your token is invalid',
-      token: token,
-      status: 'error'
-    });
-  }
-});
-
-var sendPush = function sendPush(token, title, description, response, delay) {
-  var delayPushNotification = delay || 0;
-
-  setTimeout(function () {
-    expo.sendPushNotificationAsync({
-      // The push token for the app user you want to send the notification to
-      to: token,
-      sound: 'default',
-      title: title || 'Push notification title',
-      body: description || 'Push notification description',
-      data: {
-        title: title,
-        description: description
-      }
-    }).then(function (res) {
-      response.json({
-        icon: '✅',
-        message: title,
-        description: description,
-        token: token,
-        status: 'sent',
-        res: res
-      });
-    }, function (err) {
-      response.json({
-        icon: '❌',
-        message: title,
-        description: description,
-        token: token,
-        status: 'error'
-      });
-    });
-  }, delayPushNotification);
-};
-
 app.listen(port, function () {
-  console.log('\n==============================================\n[+] Server running on port: ' + port + '\n==============================================\n');
+    console.log('\n==============================================\n[+] Server running on port: ' + port + '\n==============================================\n');
 });
